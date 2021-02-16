1cf90748a7ba4746971360c1718fbd6f4cf01341
E. Hannah <echannah631@gmail.com>
2021-02-15 20:18:54 -0800

    created ibactions and iboutlets for calculate controller

---
+++ Tipsy/Controllers/TotalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TotalViewController.swift
+//  Tipsy
+//
+//  Created by Elliot Hannah III on 2/15/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TotalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
